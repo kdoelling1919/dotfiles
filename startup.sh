0be1a775cea6ba68479017f7a18cf990348126ee
kdoelling1919 <kd889@nyu.edu>
2020-02-18 16:48:40 +0100

    remove thunderbird from startup files

--- startup.sh
+++ startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # The purpose of this file is to open the applications we always do and
-# organize them a bit so we can get off the ground running 
+# organize them a bit so we can get off the ground running
 #
 # It requires the installation of wmctrl
 
@@ -9,32 +9,26 @@ sleep 6
 # Load our inital start up apps
 wmctrl -s 0
 gnome-terminal &
-thunderbird &
 slack &
-firefox www.gmail.com &
+firefox www.gmail.com webmail.pasteur.fr &
 
-count=$(wmctrl -l | grep -ic -e slack -e thunderbird -e firefox)
-check=3
+count=$(wmctrl -l | grep -ic -e slack -e firefox)
+check=2
 # Wait until all 3 applications have loaded
 while [ "$count" != "$check" ]
-do 
+do
   sleep 0.5
-  count=$(wmctrl -l | grep -ic -e slack -e thunderbird -e firefox)
+  count=$(wmctrl -l | grep -ic -e slack -e firefox)
 done
 
 # Move windows to their respective workspaces
-wmctrl -r thunderbird -t 1
+wmctrl -r slack -t 1
 while (( $(wmctrl -d | wc -l) <= 2 ))
 do
   sleep 0.5
 done
-wmctrl -r slack -t 2
+wmctrl -r firefox -t 2
 while (( $(wmctrl -d | wc -l) <= 3 ))
-do
-  sleep 0.5
-done
-wmctrl -r firefox -t 3
-while (( $(wmctrl -d | wc -l) <= 4 ))
 do
   sleep 0.5
 done
